ad360f10751c933c07822290c9686da2f00660c0
Antônio Faria Neto <110260972+AntonioF2@users.noreply.github.com>
2024-08-19 23:00:01 -0300

    Update Relatorio 01 Questao 1.vb

--- Relatorio01/Relatorio 01 Questao 1.vb
+++ Relatorio01/Relatorio 01 Questao 1.vb
@@ -9,14 +9,14 @@ INPUT "Entre com c: ", c
 delta = b * b - 4 * a * c
 
 IF delta < 0 THEN
-    PRINT "Não há raízes reais."
+    PRINT "Nao temos raizes reais."
 ELSEIF delta = 0 THEN
     x1 = -b / (2 * a)
-    PRINT "Há uma raiz real: "; x1
+    PRINT "Temos uma raiz real: "; x1
 ELSE
     x1 = (-b + SQR(delta)) / (2 * a)
     x2 = (-b - SQR(delta)) / (2 * a)
-    PRINT "Raízes: "; x1; " e "; x2
+    PRINT "Raizes: "; x1; " e "; x2
 END IF
 
 
