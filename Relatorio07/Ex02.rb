aa5f256393a967d3e7595418eac00edd901ed7ee
Antônio Faria Neto <110260972+AntonioF2@users.noreply.github.com>
2024-10-08 00:42:13 -0300

    Update Ex02.rb

--- Relatorio07/Ex02.rb
+++ Relatorio07/Ex02.rb
@@ -1,1 +1,28 @@
+class Carro
+  def descrever
+    "Este é um carro genérico."
+  end
+end
 
+class CarroEsportivo < Carro
+  def descrever
+    "Este é um carro esportivo, com alta performance e velocidade."
+  end
+end
+
+
+class CarroSedan < Carro
+  def descrever
+    "Este é um carro sedan, confortável e espaçoso."
+  end
+end
+
+# Criação das instâncias (objetos)
+carro_generico = Carro.new
+carro_esportivo = CarroEsportivo.new
+carro_sedan = CarroSedan.new
+
+# Chamando o método descrever para cada objeto
+puts carro_generico.descrever
+puts carro_esportivo.descrever
+puts carro_sedan.descrever
